f5fb14150deea6f40e0f462a3dc1ae19ae4f0883
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-19 07:33:41 +0200

    updates

--- main.js
+++ main.js
@@ -31,11 +31,13 @@ let shopItemsData = [
   },
 ]
 
-let basket = []
+let basket = JSON.parse(localStorage.getItem('data')) || []
+
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
       let { id, name, price, desc, img } = x
+      let search = basket.find((x) => x.id === id) || []
        return`
     <div id=product-id-${id} class="item">
         <img width="220" src=${img} alt="" >
@@ -46,7 +48,7 @@ let generateShop = () => {
             <h2>$ ${price}</h2>
             <div class="buttons">
               <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id=${id}class="quantity">0</div>
+              <div id=${id}class="quantity">${search.item === undefined? 0: search.item}</div>
               <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
             </div>
           </div>
@@ -69,7 +71,7 @@ let increment = (id) => {
   } else {
     search.item += 1
   }
-  //console.log(basket)
+  localStorage.setItem('data', JSON.stringify(basket))
   update(selectedItem.id)
 }
 
@@ -80,8 +82,7 @@ let decrement = (id) => {
   else {
     search.item -= 1
   }
-
-  localStorage.setItem()
+  localStorage.setItem('data', JSON.stringify(basket))
   update(selectedItem.id)
 }
 
