5df003615c13292aacf9573bcb8149ffd418b21e
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-19 21:31:46 +0200

    subsequent updates

--- src/main.js
+++ src/main.js
@@ -1,35 +1,6 @@
 let shop = document.getElementById('shop')
 
-let shopItemsData = [
-  {
-    id: 'sjjdjdhhe',
-    name: 'Casual Shirt',
-    price: 50,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/img-1.jpg'
-  },
-  {
-    id: 'shdhdhfus',
-    name: 'Office Shirt',
-    price: 120,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/img-2.jpg'
-  },
-  {
-    id: 'ajsjdjfhdj',
-    name: 'T Shirt',
-    price: 35,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/img-3.jpg'
-  },
-  {
-    id: 'shdjfjdeh',
-    name: 'Mens Suit',
-    price: 500,
-    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing.',
-    img: 'images/img-4.jpg'
-  },
-]
+
 
 let basket = JSON.parse(localStorage.getItem('data')) || []
 
