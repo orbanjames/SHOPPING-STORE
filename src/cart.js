05a6f67a6728592a35ebe0d03d140f5aed6134e2
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-20 00:03:29 +0200

    subsequent updates and submits

--- src/cart.js
+++ src/cart.js
@@ -22,16 +22,21 @@ let generateCartItems = () => {
      <img width= "100" src = ${search.img}alt="" /> 
      <div class="details">
      <div class="title-price-x">
-     <h4>
+     <h4 class"title-price">
       <p>${search.name}</p>
       <p>${search.price}</p>
      </h4>
      <i class = "bi bi-x-lg"></i>
      </div>
 
-     <div class="cart-buttons"></div>
+     <div class="buttons">
+      <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+      <div id=${id}class="quantity">${item}</div>
+      <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+     </div>
 
-     <h3></h3>
+     <h3>$ ${item * search.price}</h3>
+
      </div>   
      </div>
      `
@@ -47,3 +52,43 @@ let generateCartItems = () => {
   }
 }
 generateCartItems()
+
+let increment = (id) => {
+  let selectedItem = id
+  let search = basket.find((x) => x.id === selectedItem.id)
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    })
+  } else {
+    search.item += 1
+  }
+
+  update(selectedItem.id)
+  localStorage.setItem('data', JSON.stringify(basket))
+}
+let decrement = (id) => {
+  let selectedItem = id
+  let search = basket.find((x) => x.id === selectedItem.id)
+
+  if (search === undefined) return
+  else if (search.item === 0) return
+  else {
+    search.item -= 1
+  }
+  update(selectedItem.id)
+  basket = basket.filter((x) => x.item !== 0)
+
+  generateCartItems()
+
+  localStorage.setItem('data', JSON.stringify(basket))
+}
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id)
+  //console.log(search.item)
+  document.getElementById(id).innerHTML = search.item
+  calculation()
+}
