e10bbdbd99c24e742932ad38a00774fb7368269e
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-19 22:08:05 +0200

    subsequents commits

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,6 @@
+let label = document.getElementById('label')
+let shoppingCart = document.getElementById('shopping-cart')
+
 let basket = JSON.parse(localStorage.getItem('data')) || []
 
 let calculation = () => {
@@ -6,3 +9,16 @@ let calculation = () => {
 }
 
 calculation()
+let generateCartItems = () => {
+ if (basket.length !==0){
+
+ } else{
+  shoppingCart.innerHTML = ``;
+  label.innerHTML = ` <h2>Cart is empty
+  <a href = "index.htm">
+  <button class = "HomeBtn">Back to home</button> </a>
+  
+  `;
+ }
+}
+generateCartItems();
